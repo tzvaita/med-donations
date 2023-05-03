066e5f90a41c28ae3f63f51a55fbb0ef1d40a6ae
tzvaita <tzvaita@gmail.com>
2023-05-03 21:55:53 +0200

    Add vonage

---
+++ app/models/room.rb
@@ -0,0 +1,8 @@
+class Room < ApplicationRecord
+  before_create do
+    opentok = OpenTok::OpenTok.new(47709001, 246826d154470c432e2f35bede808d4239841b41)
+    session = opentok.create_session
+
+    self.vonage_session_id = session.session_id
+  end
+end
